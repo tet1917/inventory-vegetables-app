01447f22aff232b369515c68c58e4df59741fbdb
take1917 <tetsu0224.soccer@icloud.com>
2025-08-17 22:16:32 +0900

    user

--- backend/models/user.go
+++ backend/models/user.go
@@ -1,1 +1,8 @@
 package models
+
+type User struct {
+	ID           string `json:"id" db:"id"`
+	Name         string `json:"name" db:"name" validate:"required,min=1,max=100"`
+	Email        string `json:"email" db:"email" validate:"required,email"`
+	PasswordHash string `json:"-" db: "password_hash"` //json出力はしない。
+}
